c30496a1ba063d2750865492bfd0286737a8c3af
MarianSkrzypek <tsadowski@sigma.ug.edu.pl>
2014-11-07 17:07:22 +0100

    Update zad4.c

--- zad4.c
+++ zad4.c
@@ -3,15 +3,15 @@
 int main()
 {
 int p;
-printf("Podaj liczbe calkowita");
+printf("Podaj liczbe calkowita\n");
 scanf("%d",&p);
 if(p>=0)
   {
-    printf("liczba ta jest dodania");
+    printf("liczba ta jest dodania\n");
   }
   else
     {
-      printf("liczba ta jest ujemna");
+      printf("liczba ta jest ujemna\n");
     }
 return 0;
 }
