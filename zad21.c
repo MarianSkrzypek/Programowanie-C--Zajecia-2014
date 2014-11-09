18c223d93696a931d7a17911bbe8aacd8584ed36
MarianSkrzypek <tsadowski@sigma.ug.edu.pl>
2014-11-09 09:47:39 +0100

    Update zad21.c

--- zad21.c
+++ zad21.c
@@ -2,25 +2,25 @@
 double a,b;
 double W(a,b){
 if(a>b){
-return a;
+    return a;
 }
-else{
-return b;
+        else{
+            return b;
 }
 }
-double M(a,b){
+    double M(a,b){
 if(a<b){
-return a;
+        return a;
 }
 else{
-return b;
+        return b;
 }
 }
-double praw(a,b){
-return (W(a,b)-M(a,b))/(W(a,b)+M(a,b));
+    double praw(a,b){
+        return (W(a,b)-M(a,b))/(W(a,b)+M(a,b));
 }
 int main()
 {
-printf("wartosc praw= %lf",praw(3,1));
-    return 0;
+        printf("wartosc praw= %lf",praw(3,1));
+            return 0;
 }
